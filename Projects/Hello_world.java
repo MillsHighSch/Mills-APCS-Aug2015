160bb424f0907079d27ca86e31ba0cc1f5f7cdcb
Lamontsm <steven@lamonaco.com>
2015-07-13 17:29:57 -0700

    13 July Hello world demo
    my first program

---
+++ Projects/Hello_world.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hello_world
+{
+    // instance variables - replace the example below with your own
+    
+
+    
+        // initialise instance variables
+      public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Steve"); //Display the string
+   
+
+    }
+
+    
+}
