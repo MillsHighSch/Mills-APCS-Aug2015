7aa21b0745858d70090ba2a154b5378df3b800b6
Lamontsm <steven@lamonaco.com>
2015-07-28 16:11:18 -0700

    Completed and submitted FracCalc project
    Did not run the testers.

--- FracCalc/FracCalc.java
+++ FracCalc/FracCalc.java
@@ -5,7 +5,7 @@ public class FracCalc {
 
     public static void main(String[] args) 
     {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+        // Read the input from the user and call produceAnswer with an equation
         Scanner console = new Scanner(System.in);
 
         boolean is_quit = false;
@@ -49,6 +49,9 @@ public class FracCalc {
         
         // Find and convert / as operator to # to remove ambiguity
         input = input.replace(" / "," # ");
+        
+        // Find and convert - as operator to $ to remove ambiguity
+        input = input.replace(" - "," $ ");
 
         //Remove spaces from the string
         for (int k=0;k<input.length();k++){
@@ -62,21 +65,24 @@ public class FracCalc {
         parsed = parse_input(input);
         String operator = parsed [0];
         
-        //print out a check. TODO: remove this for final
-        System.out.println(parsed[1] + "..." + operator + "..." + parsed[2]);
-        
-        //Convert each fraction to integers strings -- whole number,numerator, denominator
+        if (operator.contains("$")){operator = "-";}
+        if (operator.contains("#")){operator = "/";}
+               
+        //Convert each fraction to integers arrays -- whole number,numerator, denominator
        int [][] values = new int [2][3];
+       
        values [0] = parse_fraction(parsed[1]);
        values [1] = parse_fraction(parsed[2]);
-       //TODO : remoove for final.
-       System.out.println("first whole: " + values[0][0]+ ", first numer: " + values[0][1]+ ", first denom: " + values  [0][2]);
-       System.out.println("second whole: " + values[1][0]+ ", 2nd numer: " + values[1][1]+ ", 2nd denom: " + values  [1][2]);
        
        //Render both numbers as improper and unreduced fractions
        for (int k=0;k<2;k++){
            if (values[k][2]!=0){
-               values[k][1]=values[k][1]+(values[k][0]*values[k][2]);
+               if (values[k][0]<0){
+                   values[k][1]=(values[k][0]*values[k][2])-values[k][1];
+                }
+                else{
+                    values[k][1]=values[k][1]+(values[k][0]*values[k][2]);
+                }
                values[k][0]=0;
             }
             else{
@@ -107,33 +113,40 @@ public class FracCalc {
         //pull out whole number
         results[0] = results[1]/results[2];
         results[1] = results[1] % results[2];
-        
-         //temporary print out. remove in final
-        System.out.println("whole =" + results[0] + "; numer = " + results [1] + "; denom = " + results [2]);
-        
-        //TODO: reduce the fraction
-
-        return "";
+        if (results[1]<0){results[1] = results[1] * -1;} //Correct for stray negatives in num or denom
+        if (results[2]<0){results[2] = results[2] * -1;}
+       
+        //reduce the fraction
+        if (results[1] == 0){
+            return Integer.toString(results[0]); // is just a whole number
+        }
+        if (results[2] < 0){ //negative in wrong place
+            results[2] = results[2] * -1;
+            results[1] = results[1] * -1;
+        }
+        if (results[1] < 0){ //fraction is negative
+            results[0] = results[0] * -1;
+            results[1] = results[1] * -1;
+        }
+        for (int i=2;i<= (1+results[1]/2);i++){ //Look for common denominators
+                if ((results[1] % i == 0) && (results[2] % i == 0)){
+                    results[1]=results[1]/i;
+                    results[2]=results[2]/i;
+                    i--;
+                }
+        }
+        if (results[0] == 0){
+            return results[1] + "/" + results[2];// There is no whole number portion
+        }
+        return results[0] + "_" + results[1] + "/" + results[2];
     }
-
-     // This is driver for writing test routines
-    public static void tester(){
-        int [][] number = new int [3][3];
-        int []result = new int [3];
-        number [0][1]= 5;
-        number [0][2]= 8;
-        number [1][1]= 6;
-        number [1][2]= 16;
-        result = add_subt(number);
-        System.out.println(result[0] + "..." + result[1] + "..." + result[2]);
-        
-}
 
 // Returns string with operator, first fraction, second fraction strings
 public static String[] parse_input(String input){
     String [] parsed = new String [3];
+  
     for (int i=0;i<input.length();i++){
-            if (input.charAt(i)=='+' || input.charAt(i)=='-' || input.charAt(i)=='*' || input.charAt(i)=='#'){
+            if (input.charAt(i)=='+' || input.charAt(i)=='$' || input.charAt(i)=='*' || input.charAt(i)=='#'){
                 parsed[0] = input.substring(i,i+1);
                 parsed[1] = input.substring(0,i);
                 parsed[2] = input.substring(i+1,input.length());
@@ -164,7 +177,6 @@ public static int [] parse_fraction(String input){
         
         //Now look for fraction parts
         if (input.contains("/")){
-            System.out.println(input);
             int i = input.indexOf("/");
             values[1] = Integer.parseInt(input.substring(0,i));
             values[2] = Integer.parseInt(input.substring(i+1,input.length()));
