b4ec01d6f04ffb89d7172e592b2daf4bd5761151
Lamontsm <steven@lamonaco.com>
2016-01-20 11:29:12 -0800

    upgraded Golf to 2 golfers

--- Golf/Golf.java
+++ Golf/Golf.java
@@ -8,11 +8,17 @@
 public class Golf
 {
     public static void main(String[] args){
-        ScoreKeeper golfer = new ScoreKeeper();
-        golfer.holeScore(1,5,4);
-        golfer.holeScore(2, 3,5);
-        golfer.holeScore(3, 4,5);
-        golfer.holeScore(4, 3,2);
-        golfer.finalTally();
+        ScoreKeeper golfer1 = new ScoreKeeper("Fred");
+        ScoreKeeper golfer2 = new ScoreKeeper("Wilma");
+        golfer1.holeScore(1,5,4);
+        golfer2.holeScore(1,5,5);
+        golfer1.holeScore(2, 3,5);
+        golfer2.holeScore(2,3,5);
+        golfer1.holeScore(3, 4,5);
+        golfer2.holeScore(3,4,2);
+        golfer1.holeScore(4, 3,2);
+        golfer2.holeScore(4,3,3);
+        golfer1.finalTally();
+        golfer2.finalTally();
     }
 }
--- Golf/ScoreKeeper.java
+++ Golf/ScoreKeeper.java
@@ -10,32 +10,37 @@ public class ScoreKeeper
     // initialize cumulative scores to zero
     int cumulScore = 0;
     int cumulPar = 0;
+    String name;
     String results[] = {"horrible","double bogey", "bogey", "par", "birdie",
-        "eagle", "fantastic"};
+            "eagle", "fantastic"};
+    public ScoreKeeper(String golferName){
+        name = golferName;
+    }
 
     /*
      * use results from each hole, add to cumulative totals, then print relative to par 
      * for each hole
      */
     void holeScore(int hole, int par, int score){
-      this.cumulScore += score;
-      this.cumulPar += par;
-      if (score-par > 2) System.out.println("Hole " + hole + ": " + results[0]); // for bad results
-      else if (par-score >2) System.out.println("Hole " + hole + ": " + results[6]);
-      else System.out.println("Hole " + hole + ": " + results[(par-score) + 3]);
+        this.cumulScore += score;
+        this.cumulPar += par;
+        if (score-par > 2) System.out.println("For golfer " + this.name + " Hole " + hole + ": " + results[0]); // for bad results
+        else if (par-score >2) System.out.println("For golfer " + this.name + " Hole " + hole + ": " + results[6]);
+        else System.out.println("For golfer " + this.name + " Hole " + hole + ": " + results[(par-score) + 3]);
     }
+
     /*
      * Use the final tallies to print out how the golfer did on the entire round.
      */
     void finalTally(){
         if (cumulScore > cumulPar){
-            System.out.println("Overall you shot " + (cumulScore-cumulPar) +
-            " over par");
+            System.out.println("Overall " + this.name + " shot " + (cumulScore-cumulPar) +
+                " over par");
         }
         else if (cumulScore < cumulPar){
-            System.out.println("Overall you shot " + (cumulPar-cumulScore) +
-            " under par");
+            System.out.println("Overall " + this.name + " shot " + (cumulPar-cumulScore) +
+                " under par");
         }
-        else System.out.println("Overall you shot par");
+        else System.out.println("Overall " + this.name + " shot par");
     }
 }
