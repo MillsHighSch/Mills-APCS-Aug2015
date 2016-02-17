612b1fed28f245a7d57e2a81fcac086c08156e8b
Lamontsm <steven@lamonaco.com>
2016-02-16 20:40:24 -0800

    some updates on OOP demo

--- OOP class trials/AnimalRunner.java
+++ OOP class trials/AnimalRunner.java
@@ -9,8 +9,14 @@ public class AnimalRunner
         marthasDog = marysDog;
         // note that == works for instance variables within an object
         System.out.println("check values equal: " + (marthasDog.age == davidsDog.age));
+        // Try == for objects
+        System.out.println("check objects equal == " + (marysDog == marthasDog));
+        System.out.println("check different ojects equal == " + (marysDog == davidsDog));
         // we need the .equals to compare objects to each other
         System.out.println("check objects equal: " + marthasDog.equals(marysDog));
+        /* although their values are the same, davidsDog is not equal to marysDog
+         * it is like the analogy of two people having similar pairs of shoes.
+         */
         System.out.println("check objects equal: " + davidsDog.equals(marysDog));
         // Try out whether super can look up 2 levels
         davidsDog.methodRunner();
